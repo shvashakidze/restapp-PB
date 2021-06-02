4e0f1cab29e27cf25bde7c0b83a7073d4d615346
shvashakidze <shvashakidze@users.noreply.github.com>
2021-06-02 12:52:10 -0700

    cors, morgan

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
+app.use(morgan('tiny'))
+app.use(express.static('build'))
 
 let persons =  [
   {
