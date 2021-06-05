0acfbaaf258d686f4b2c303d7c5dcebeaad44bda
shvashakidze <shvashakidze@users.noreply.github.com>
2021-06-05 10:13:15 -0700

    heroku push

--- index.js
+++ index.js
@@ -107,6 +107,7 @@ let persons =  [
     response.json(person)
   })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
